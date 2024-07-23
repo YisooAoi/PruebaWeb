0704282fa487a1c568a70141cd918f6b6a9d77b5
YisooAoi <YisooAoi@proton.me>
2024-07-22 23:35:16 -0300

    Got onpage editing function to be called properly PD:  Learn to fricking write like a decent human being calling it for today

--- comunicacionBaseDeDatos/javascript/editor.js
+++ comunicacionBaseDeDatos/javascript/editor.js
@@ -1,6 +1,18 @@
-const parametros = new URLSearchParams(document.location.search);
-const identificador = parametros.get("identificador");
-const formulario = document.getElementById('formularioEditor')
-if (identificador!=null) {
-  formulario.action=`comunicacionBaseDeDatos/php/modificarLink.php?identificador=${identificador}`;
+function editar(object) {
+    codigoHTML = `
+    <tr id="${object.nombre}">
+      <td class="itemLista"> ${object.nombre} </td>
+      <td class="itemLista"> ${object.link} </td>
+      <td class="itemLista"> ${object.fechaDeModificacion} </td>
+      <td class="itemLista"> ${object.imagen} </td>
+      <!-- Acciones -->
+      <td class="itemLista botones">
+        <div class="botonTabla" id="editar" onclick=
+        'editar.js('` + `$` + `{fila.nombre}` + `')' > 
+          <button class="botonSVG" alt="Editar"> 
+        </div>
+      </td>
+    </tr>`;
+    document.getElementById(object);
+    document.getElementById(object).innerHTML = codigoHTML;
 }
